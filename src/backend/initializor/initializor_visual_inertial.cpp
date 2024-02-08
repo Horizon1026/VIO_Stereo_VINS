c532edffb0878f307e929d18cd47d0758c2f311e
Horizon1026 <lijiapeng_atlantis@163.com>
2024-02-08 09:59:36 +0800

    update code

--- src/backend/initializor/initializor_visual_inertial.cpp
+++ src/backend/initializor/initializor_visual_inertial.cpp
@@ -3,6 +3,20 @@
 namespace VIO {
 
 bool Backend::EstimateGyroBias() {
+    // Preintegrate all imu measurement block.
+    for (auto &frame_with_bias : data_manager_->frames_with_bias()) {
+        RecomputeImuPreintegrationBlock(Vec3::Zero(), Vec3::Zero(), frame_with_bias);
+        // Debug.
+        frame_with_bias.imu_preint_block.SimpleInformation();
+    }
+
+    // Construct incremental function.
+    Mat3 hessian = Mat3::Zero();
+    Vec3 bias = Vec3::Zero();
+    for (auto it = data_manager_->frames_with_bias().cbegin(); std::next(it) != data_manager_->frames_with_bias().cend(); ++it) {
+        const auto &frame_i = *it;
+        const auto &frame_j = *std::next(it);
+    }
 
     return true;
 }
