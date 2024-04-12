d069934a7e5ec9cfcd7010a83fd8414adbcbcf15
Horizon1026 <lijiapeng_atlantis@163.com>
2024-04-13 00:15:46 +0800

    update code

--- run.sh
+++ run.sh
@@ -5,6 +5,7 @@ dataset_subdir="MH_01_easy"
 # MH_03_medium
 # MH_05_difficult
 
+# For ubuntu22.04
 dataset_root_dir="/media/horizon/Documents/My_Github/Datasets/${dataset_subdir}"
 if [ -d "${dataset_root_dir}" ]; then
     ./test_vio "${dataset_root_dir}/"
@@ -12,7 +13,16 @@ else
     echo ">> Failed to find ${dataset_root_dir}/"
 fi
 
+# For windows
 dataset_root_dir="/d/My_Github/Datasets/${dataset_subdir}"
+if [ -d "${dataset_root_dir}" ]; then
+    ./test_vio "${dataset_root_dir}/"
+else
+    echo ">> Failed to find ${dataset_root_dir}/"
+fi
+
+# For windows(wsl2) ubuntu20.04
+dataset_root_dir="/mnt/d/My_Github/Datasets/${dataset_subdir}"
 if [ -d "${dataset_root_dir}" ]; then
     ./test_vio "${dataset_root_dir}/"
 else
