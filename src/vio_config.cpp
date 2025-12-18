7bd9f3f0e7e4816ef7a9701bbd04d1c48ee89b83
Horizon1026 <lijiapeng_atlantis@163.com>
2025-12-18 22:54:37 +0800

    update code

--- src/vio_config.cpp
+++ src/vio_config.cpp
@@ -83,7 +83,7 @@ bool Vio::ConfigComponentOfDataLoader() {
 }
 
 bool Vio::ConfigComponentOfFrontend() {
-    using CameraType = sensor_model::CameraPinholeRadtan;
+    using CameraType = sensor_model::CameraPinholeRadialTangential;
     using FeatureType = feature_detector::FeaturePointFastDetector;
     using KltType = feature_tracker::OpticalFlowBasicKlt;
 
